94cf691761dd486a80243b3e156b66f33a9a354b
FrankFang <frankfang1990@gmail.com>
2023-05-10 16:43:26 +0800

    创建登录的测试代码

---
+++ test/controller_test/create_session_test.go
@@ -0,0 +1,30 @@
+package controller_test
+
+import (
+	"encoding/json"
+	"mangosteen/internal/router"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateSession(t *testing.T) {
+	r := router.New()
+	w := httptest.NewRecorder()
+	j := gin.H{
+		"email": "",
+		"code":  "",
+	}
+	bytes, _ := json.Marshal(j)
+	req, _ := http.NewRequest(
+		"POST",
+		"/api/v1/session",
+		strings.NewReader(string(bytes)),
+	)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+}
