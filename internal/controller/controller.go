973f8b62cd049f15d620ad5d7d834af1198d30bc
FrankFang <frankfang1990@gmail.com>
2023-05-12 22:34:13 +0800

    创建 SessionController，它实现了 Controller 接口

---
+++ internal/controller/controller.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type Controller interface {
+	Create(c *gin.Context)
+	Destroy(c *gin.Context)
+	Update(c *gin.Context)
+	Get(c *gin.Context)
+	GetPaged(c *gin.Context)
+}
---
+++ internal/controller/session_controller.go
@@ -0,0 +1,26 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type SessionController struct {
+}
+
+func (ctrl *SessionController) Create(c *gin.Context) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ctrl *SessionController) Destroy(c *gin.Context) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ctrl *SessionController) Update(c *gin.Context) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ctrl *SessionController) Get(c *gin.Context) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ctrl *SessionController) GetPaged(c *gin.Context) {
+	panic("not implemented") // TODO: Implement
+}
